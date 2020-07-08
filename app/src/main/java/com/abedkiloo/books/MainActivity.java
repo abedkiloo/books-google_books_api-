6cf88b5a883331440df9cfe13336e2b9d61d57d7
Abednego Wambua <abedxh@gmail.com>
2020-07-09 01:17:09 +0300

    adding async task for url query

--- app/src/main/java/com/abedkiloo/books/MainActivity.java
+++ app/src/main/java/com/abedkiloo/books/MainActivity.java
@@ -1,6 +1,8 @@
 package com.abedkiloo.books;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,15 +18,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         URL url = null;
-        TextView textViewResponse = findViewById(R.id.tvResponse);
         try {
             url = ApiUtil.books_url_builder("cooking");
-            String jsonResult = ApiUtil.getJson(url);
-            textViewResponse.setText(jsonResult);
+            new BookQueryTask().execute(url);
         } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public class BookQueryTask extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchURL = urls[0];
+            String result = null;
+            try {
+                result = ApiUtil.getJson(searchURL);
+            } catch (IOException e) {
+                Log.e("ERROR_JSON_REQUEST", e.getMessage());
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            TextView textViewResponse = findViewById(R.id.tvResponse);
+            textViewResponse.setText(s);
+
+        }
+    }
+
 }
